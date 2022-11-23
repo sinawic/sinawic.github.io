84e2ae44cc2350fc6961fad9ef07f69727252e74
sinawic <sina_041_sm@yahoo.com>
2022-11-23 12:34:20 +0330

    first commit

---
+++ _next/static/IkCQqmkQAvDSeqjGaU2l-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/807-8ad8823f14ee8c74.js","static/css/87ee7642001629d2.css","static/chunks/pages/index-5e8b2885f30edd42.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
